b44df4452842ae8c946f52e043a5d937f12285d9
Elena Diniz <elena.diniz@outlook.com>
2021-08-22 20:09:31 -0300

    Primeiras configurações do projeto
    - Primeiras telas construidas em Storyboard
    - Inserida arquitetura no projeto (MVC)

---
+++ HogwartsApp/HogwartsApp/View/MainViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  HogwartsApp
+//
+//  Created by Elena Diniz on 22/08/21.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var viewMain: UIView!
+    @IBOutlet weak var logoImageVIew: UIImageView!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tappedLoginButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tappedSignInButton(_ sender: UIButton) {
+    }
+}
+
